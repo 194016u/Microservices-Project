c68885240ac3ea02a8930a46919ba33f245b624d
VIRANGA BANDARA <virangabandara97@gmail.com>
2024-01-02 22:44:08 +0530

    Initial commit

---
+++ department-service/src/main/java/com/cooldeveloper/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.cooldeveloper.departmentservice.repository;
+
+import com.cooldeveloper.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
